89d095ca3d510d3d5fd3e46c8d2fce0557d3fd76
Rothy <jahanrothy58@gamil.com>
2022-05-16 23:14:54 +0600

    onclick button-events

---
+++ main.js
@@ -0,0 +1,11 @@
+let popup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open");
+}
+
+function closePopup(){
+    popup.classList.remove("open");
+}
+openPopup();
+closePopup();
